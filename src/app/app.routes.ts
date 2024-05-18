ef63977445ffa352452b3069600fe2d48deb29d9
SamirCharleston <samircharleston@gmail.com>
2024-05-18 13:10:43 -0300

    criando o espaço para as rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,26 @@ import { Routes } from '@angular/router';
 import { CachorrolistComponent } from './components/cachorro/cachorrolist/cachorrolist.component';
 
 export const routes: Routes = [
-    { path: 'cachorro', component: CachorrolistComponent, pathMatch: 'full' },
+    { 
+        path: 'cachorro', component: CachorrolistComponent, pathMatch: 'full'
+
+        //Samir insira suas rotas abaixo dessa linha
+
+        //Samir insira suas rotas acima dessa linha
+
+
+        //Cassiano insira suas rotas abaixo dessa linha
+
+        //Cassiano insira suas rotas acima dessa linha
+
+
+        //Luan insira suas rotas abaixo dessa linha
+
+        //Luan insira suas rotas acima dessa linha
+
+
+        //Joao insira suas rotas abaixo dessa linha
+
+        //Joao insira suas rotas acima dessa linha
+    },
 ];
