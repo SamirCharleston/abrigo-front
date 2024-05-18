276576e7d95f2a7cfdf3b7d0534e3d7b576ef75b
SamirCharleston <samircharleston@gmail.com>
2024-05-18 14:02:37 -0300

    salvando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,30 @@
+//Samir insira seus imports abaixo dessa linha
 import { Routes } from '@angular/router';
 import { LoginComponent } from './layouts/login/login/login.component';
+//Samir insira seus imports acima dessa linha
+
+
+//Cassiano insira seus imports abaixo dessa linha
+
+//Cassiano insira seus imports acima dessa linha
+
+
+//Luan insira seus imports abaixo dessa linha
+
+//Luan insira seus imports acima dessa linha
+
+
+//Joao insira seus imports abaixo dessa linha
+
+//Joao insira seus imports acima dessa linha
+
 
 export const routes: Routes = [
     
     //Samir insira suas rotas abaixo dessa linha
-    {path: '', component: LoginComponent, pathMatch: 'full'},
-    { path: 'login', component: LoginComponent}
+    { path: '', redirectTo: 'login', pathMatch: 'full'},
+    { path: 'login', component: LoginComponent},
+    { path: 'home', }
     //Samir insira suas rotas acima dessa linha
 
 
--- src/app/layouts/login/login/login.component.ts
+++ src/app/layouts/login/login/login.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, MdbFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  usuario!: string;
+  senha!: string;
 
+  router = inject(Router);
+
+  logar() {
+    if (this.usuario == 'admin' && this.senha == 'admin') {
+      this.router.navigate(['/home']);
+    } else
+      alert('Usuário ou senha estão incorretos!');
+  }
 }
