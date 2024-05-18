b162c0421cd5822b7cfa73dce1ec8cd472a09232
SamirCharleston <samircharleston@gmail.com>
2024-05-18 13:27:17 -0300

    salvando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,26 +2,25 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './layouts/login/login/login.component';
 
 export const routes: Routes = [
-    { 
-        
-        //Samir insira suas rotas abaixo dessa linha
-        path: '', component: LoginComponent, pathMatch: 'full'
-
-        //Samir insira suas rotas acima dessa linha
-
-
-        //Cassiano insira suas rotas abaixo dessa linha
-
-        //Cassiano insira suas rotas acima dessa linha
+    
+    //Samir insira suas rotas abaixo dessa linha
+    {path: '', component: LoginComponent, pathMatch: 'full'},
+    { path: 'login', component: LoginComponent}
+    //Samir insira suas rotas acima dessa linha
 
 
-        //Luan insira suas rotas abaixo dessa linha
+    //Cassiano insira suas rotas abaixo dessa linha
 
-        //Luan insira suas rotas acima dessa linha
+    //Cassiano insira suas rotas acima dessa linha
 
 
-        //Joao insira suas rotas abaixo dessa linha
+    //Luan insira suas rotas abaixo dessa linha
 
-        //Joao insira suas rotas acima dessa linha
-    },
+    //Luan insira suas rotas acima dessa linha
+
+
+    //Joao insira suas rotas abaixo dessa linha
+
+    //Joao insira suas rotas acima dessa linha
+
 ];
