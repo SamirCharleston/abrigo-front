b1ec6267ec56142e4070dd32d97a8d2d2b45c144
SamirCharleston <samircharleston@gmail.com>
2024-05-19 20:25:59 -0300

    desenvolvendo a lista de requerimentos

--- src/app/components/requerimento/requerimentolist/requerimentolist.component.ts
+++ src/app/components/requerimento/requerimentolist/requerimentolist.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Requerimento } from '../../../models/requerimento/requerimento';
+import { Tutor } from '../../../models/tutor/tutor';
+import { Cachorro } from '../../../models/cachorro/cachorro';
 
 @Component({
   selector: 'app-requerimentolist',
@@ -8,5 +11,8 @@ import { Component } from '@angular/core';
   styleUrl: './requerimentolist.component.scss'
 })
 export class RequerimentolistComponent {
+  requerimentos!: Requerimento[];
 
+  constructor() {
+  }
 }
--- src/app/models/requerimento/requerimento.ts
+++ src/app/models/requerimento/requerimento.ts
@@ -1,2 +1,8 @@
+import { Cachorro } from "../cachorro/cachorro";
+import { Tutor } from "../tutor/tutor";
+
 export class Requerimento {
+    id!: number;
+    tutor!: Tutor;
+    cachorros!: Cachorro[];
 }
